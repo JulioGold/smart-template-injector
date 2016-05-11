610260e6f6d2e7da47ea67a1af8c58092c131c3c
JulioGold <julio.gold@gmail.com>
2016-05-11 16:33:29 -0300

    Fix inject function to respect line breaks.

--- smartTemplateInjector.js
+++ smartTemplateInjector.js
@@ -12,24 +12,19 @@ function Inject(template, contextObj) {
 		return template;
 	}
 
+	var os = os || require('os');
+
 	// This get the tags by /*inject:*/ with one group
 	//return template.replace(/\/\*inject:([\w\s]*)\*\//igm,
 	// This get the tags by /*inject:*/ with two groups. | Como tem 2 grupos, p1 é o primeiro e p2 o segundo
-	return template.replace(/^(\s*?)\/\*inject:([\w\.\s]*)\*\//igm,
+	return template.replace(/([\t ]*)\/\*inject:([\w\.\s]*)\*\//ig,
  
 		function (str, p1, p2, offset, s) {
 			
 			var retorno = deepFind(contextObj, p2);
 			
-			var initLine = p1;
-			
-			// É necessário verificar se este teste não ficou uma gambiarra, talvez deveria ser feito através do os.EOL
-			if (p1 == '\n' || p1 == '\n\r') {
-				initLine = "";
-			}
-
 			if (retorno != null) {
-				return initLine + retorno + str;
+				return p1 + retorno + os.EOL + str;
 			} else {
 				return str;
 			}
